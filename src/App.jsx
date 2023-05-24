656b964a033a0465edd8ed472af40502fb4d21a5
Daijoubust <s-t-a-m-p-2@hotmail.com>
2023-05-25 01:21:27 +0700

    Pokemon Randomizer ver.1.0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Home from './components/Home'
+import './app.scss'
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="center">
+        <Home/>
+      </div>
+    </div>
+  )
+}
+
+export default App
